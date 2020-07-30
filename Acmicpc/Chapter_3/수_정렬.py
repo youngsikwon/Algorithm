5f12507e5abedc94dbc64116e3b33a6322dfc896
youngsik <youngsik3200@gmail.com>
2020-07-30 14:56:34 +0900

    pytohn 알고리즘 공부

--- Acmicpc/Chapter_3/수_정렬.py
+++ Acmicpc/Chapter_3/수_정렬.py
@@ -9,4 +9,27 @@ data.sort()
 
 for x in data:
     print(x)
+#####################################
+
+
+n1 = int(input())
+
+array = list()
+
+for _ in range(n1):
+    array.append(int(input()))
+
+
+for i in range(n1):
+    min_index = i # 가장 작은 원소의 인덱스
+    for j in range(i + 1, n1):
+        if array[min_index] > array[j]:
+            min_index = j
+            array[i], array[min_index] = array[min_index], array[i] # tmdhkvm
+
+
+for i in array:
+    print(i)
+
+
 
