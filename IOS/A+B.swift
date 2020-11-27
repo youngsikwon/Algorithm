0bb8136ebd593c6e63c730e26483973006069116
Yongsik Won <youngsik3200@gmail.com>
2020-11-27 07:57:21 +0000

    A + B / MVC 이론

--- IOS/A+B.swift
+++ IOS/A+B.swift
@@ -1,9 +1,19 @@
 import Foundation
 
 
-let value = readLine()!
+let value = readLine()! // 이것을 옵셔널을 받게 된다. 
 
+
+// spring 값이기 때문에 일반적으로 
 let array = value.components(separatedBy: " ")
 
 
 print("\(Int(array[0])! + Int(array[1])!)")
+
+// swift는 MVC 패턴이 있다.
+// MVC 패턴으로 IOS 기반 코딩을 한다.
+
+// View
+func inputView() -> String {
+  return 
+}
