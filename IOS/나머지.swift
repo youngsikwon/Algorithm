20428fb3b781f4318836a5c60042aa4365be91fa
Yongsik Won <youngsik3200@gmail.com>
2020-12-14 08:13:15 +0000

    BackJun A_ Plis

---
+++ IOS/나머지.swift
@@ -0,0 +1,10 @@
+let line = readLine() ?? "" 
+let lineArr = line.split{$0 == " "} 
+let a = Int(lineArr[0]) ?? 0 
+let b = Int(lineArr[1]) ?? 0 
+let c = Int(lineArr[2]) ?? 0 
+print((a+b)%c) 
+print((a%c + b%c)%c) 
+print((a*b)%c) 
+print(((a%c) * (b%c))%c)
+
